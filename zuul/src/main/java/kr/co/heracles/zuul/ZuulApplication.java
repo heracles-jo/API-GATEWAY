582c2b0b25964d111587df0514e58b4e10e9a1c9
heracles-jo <heracles.jo@gmail.com>
2019-12-25 22:49:05 +0900

    Default Setting Add, Project Name Change.

--- zuul/src/main/java/kr/co/heracles/zuul/ZuulApplication.java
+++ zuul/src/main/java/kr/co/heracles/zuul/ZuulApplication.java
@@ -2,7 +2,11 @@ package kr.co.heracles.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class ZuulApplication {
 
